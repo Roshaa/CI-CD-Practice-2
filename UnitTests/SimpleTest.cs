44f706efc05d842e6b154eb9d937286417407780
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2025-11-01 17:58:53 +0000

    added tests

---
+++ UnitTests/SimpleTest.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTests
+{
+    public class SimpleTest
+    {
+        [Fact]
+        public void TestAddition()
+        {
+            int a = 5;
+            int b = 10;
+            int sum = a + b;
+            Assert.Equal(15, sum);
+        }
+    }
+}
