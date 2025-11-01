3b55c5114295d5f1588557c360166f291e961025
Ricardo Rocha <48363753+Roshaa@users.noreply.github.com>
2025-11-01 17:10:37 +0000

    Add project files.

---
+++ CI CD Practice 2/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_CD_Practice_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+
+    }
+}
